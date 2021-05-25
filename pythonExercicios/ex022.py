0dbb79c6d90a7cafde7dcd2031cf4914fa938b57
liconeto <benicio.neto9@gmail.com>
2021-05-25 00:01:12 -0300

    exe 022 023

--- pythonExercicios/ex022.py
+++ pythonExercicios/ex022.py
@@ -1,10 +1,10 @@
 texto = ' Desafio 022 '
 print(' {:*^30}'. format(texto))
-nome = str(input('Digite o nome completo : '))
+nome = str(input('Digite o nome completo : ')).strip()
 print('O nome digitado foi : ', nome)
 print('Em maiúsculo : ', nome.upper())
 print('Em minusculo :', nome.lower())
 print('tamanho com espaço : ', len(nome))
-print('tamanho sem espaço : ', len(nome.strip()))
+print('tamanho sem espaço : ', len(nome) - nome.count(' '))
 primeiro = nome.split()
 print('O primeiro nome tem {} letras' .format(len(primeiro[0])))
--- pythonExercicios/ex023.py
+++ pythonExercicios/ex023.py
@@ -1,11 +1,12 @@
 texto = ' Desafio 023 '
 print(' {:*^30}'. format(texto))
-numero = str(input('Digite um valor de 0 à 9999 : '))
-
-separado = numero.split( maxsplit=3)
-print('Valor', separado)
-'''print('O Valor digitado foi : ', numero)
-print(' Unidades : ', numero / 1)
-print(' Dezenas : ', numero / 10)
-print(' Centenas : ', numero / 100)
-print(' Milhar : ', numero / 1000)'''
+numero = int(input('Digite um valor de 0 à 9999 : '))
+u = numero // 1 % 10
+d = numero // 10 % 10
+c = numero // 100 % 10
+m = numero // 1000 % 10
+print('Analisando o número : {}' .format(numero))
+print('Unidade : {}' .format(u))
+print('Dezena : {}' .format(d))
+print('Centena : {}' .format(c))
+print('Milhar : {}' .format(m))
