765cb8e28196b5a8cd7ddd988f0cff7bb686ae8d
liconeto <benicio.neto9@gmail.com>
2021-05-24 23:29:28 -0300

    exe023

---
+++ pythonExercicios/ex023.py
@@ -0,0 +1,11 @@
+texto = ' Desafio 023 '
+print(' {:*^30}'. format(texto))
+numero = str(input('Digite um valor de 0 à 9999 : '))
+
+separado = numero.split( maxsplit=3)
+print('Valor', separado)
+'''print('O Valor digitado foi : ', numero)
+print(' Unidades : ', numero / 1)
+print(' Dezenas : ', numero / 10)
+print(' Centenas : ', numero / 100)
+print(' Milhar : ', numero / 1000)'''
---
+++ pythonExercicios/ex024.py
@@ -0,0 +1,8 @@
+texto = ' Desafio 024 '
+print(' {:*^30}'. format(texto))
+cidade = input('Digite o nome da cidade : ')
+
+cidade = cidade.split()
+
+print('A cidade digiatada foi : {} \n'
+      'e ela começa com : {}'. format(cidade, cidade[0]))
