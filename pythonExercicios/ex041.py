2904e6a287114a3d35d60b451b1c54bd579ddea3
liconeto <benicio.neto9@gmail.com>
2021-06-02 18:35:57 -0300

    ex041

--- pythonExercicios/ex041.py
+++ pythonExercicios/ex041.py
@@ -8,10 +8,13 @@ cores = {'vermelho':'\033[31m', 'verde':'\033[32m', 'amarelo':'\033[33m',
          'azul':'\033[34m', 'roxo':'\033[35m', 'ciano':'\033[36m', 'cinza':'\033[37m'}
 
 print('Confederação Nacional de Natação Curso em Vídeo')
-print('A seguir informe a data de nascimento do Atelte :')
-data = str((input('data :')))
-data = str.replace('-','/', data)
-dtn = datetime.datetime.fromisoformat(input('Informe a data de nascimentodo do Atleta :'))
+print('A seguir informe a data de nascimento do Atleta :')
+dia = input('Informe o dia :')
+mes = input('Informe o mês :')
+ano = input('Informe o ano :')
 
-print('Data {}' .format(data))
-print('Dia {}' .format(dtn.day))
+dtn = datetime.datetime.fromisoformat((ano+'-'+mes+'-'+dia))
+hoje = datetime.datetime.today()
+print('Data {}' .format(dtn.date().strftime("%d/%m/%Y")))
+print('Hoje é {}' .format(hoje.strftime("%d/%m/%Y")))
+print('{}' .format((hoje - dtn).strftime("%y")))
