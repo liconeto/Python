38233669bfc977b2f938c51c8b9dc49e6d2b7900
liconeto <benicio.neto9@gmail.com>
2021-06-10 00:19:43 -0300

    ex042

--- pythonExercicios/ex041.py
+++ pythonExercicios/ex041.py
@@ -15,6 +15,17 @@ ano = input('Informe o ano :')
 
 dtn = datetime.datetime.fromisoformat((ano+'-'+mes+'-'+dia))
 hoje = datetime.datetime.today()
-print('Data {}' .format(dtn.date().strftime("%d/%m/%Y")))
+print('Data nascimento ={}' .format(dtn.date().strftime("%d/%m/%Y")))
 print('Hoje é {}' .format(hoje.strftime("%d/%m/%Y")))
-print('{}' .format((hoje - dtn).strftime("%y")))
+idade = ((hoje - dtn).days / 365)
+print('{:.0f}' .format((hoje - dtn).days / 365))
+
+if idade <= 9:
+    print('O atleta tem {}{:.0f}{} anos e é da {}categoria Mirim' .format(cores['ciano'], idade,'\033[m', cores['vermelho']))
+elif idade >9 and idade <= 14:
+    print('O atleta tem {}{:.0f}{} anos e é da {}categoria Infantil' .format(cores['vermelho'], idade, '\033[m', cores['amarelo']))
+elif idade >14 and idade <= 19:
+    print('O Atleta tem {}{:.0f}{} anos e é da {}categoria Junior' .format(cores['azul'], idade, '\033[m', cores['roxo']))
+elif idade >= 20 and idade <= 20.9:
+    print('O Atleta tem {}{:.0f}{} anos e é da {}categoria Sênior' .format(cores['cinza'], idade, '\033[m', cores['vermelho']))
+else: print('O Atleta tem {}{:.0f}{} e é da categoria Master' .format(cores['vermelho'], idade, '\033[m', cores['ciano']))
---
+++ pythonExercicios/ex042.py
@@ -0,0 +1,29 @@
+texto = ' Desafio 041 '
+print(' {:*^30}'. format(texto))
+
+limpa = '\033[m'
+cores = {'vermelho':'\033[31m', 'verde':'\033[32m', 'amarelo':'\033[33m',
+         'azul':'\033[34m', 'roxo':'\033[35m', 'ciano':'\033[36m', 'cinza':'\033[37m'}
+
+print('\033[31mPrincipio da desigualdade triangular!\033[m')
+print('Para saber se é possivel formar o Triângulo, informe 3 retas ')
+ab = float(input('Primeira reta : '))
+cd = float(input('Segunda reta : '))
+ef = float(input('Terceira reta : '))
+
+if ab + cd > ef and cd + ef > ab and ab + ef > cd:
+    print('A soma de duas retas são maiores que a terceira: \n'
+          'soma r1 + r2 ={} > r3 = {}' .format(ab + cd, ef))
+    print('A soma de duas retas são maiores que a terceira: \n'
+          'soma r3 + r2 ={} > r1 = {}'.format(cd + ef, ab))
+    print('A soma de duas retas são maiores que a terceira: \n'
+          'soma r1 + r3 ={} > r2 = {}'.format(ab + ef, cd ))
+    print('É POSSIVEL MONTAR UM TRIÂNGULO')
+    if ab == cd == ef:
+        print('É um triângulo {}Equilátero' .format(cores['vermelho']))
+    elif ab == cd or ab == ef or cd == ef:
+        print('É um triângulo {}Isóceles' .format(cores['ciano']))
+    else:
+        print('É um triângulo {} Escaleno' .format(cores['roxo']))
+else:
+    print('Não é possivel formar o triângulos')
