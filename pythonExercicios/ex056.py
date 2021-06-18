c2e263020d250d64fd7e9942f7f337a46d754e28
liconeto <benicio.neto9@gmail.com>
2021-06-17 23:26:24 -0300

    ex056

--- pythonExercicios/ex056.py
+++ pythonExercicios/ex056.py
@@ -13,21 +13,23 @@ sidade = 0
 hvelho =''
 hidade =0
 m20 =0
-for c in range(0,4):
+for c in range(1,5):
+    print('----- {}ª PESSOA ----'. format(c))
     nome = str(input('\033[34m Informe o nome :'))
     idade = int(input('\033[36m Informe a idade :'))
-    sexo = int(input('Informeo sexo : 1 = Feminico | 2 = Masculino :'))
+    sexo = int(input('\033[36m Informe o sexo : 1 = Feminino | 2 = Masculino :\033[m'))
 
     sidade += idade
+    print('nome = {} | idade = {} | sexo = {}' .format(nome, idade, sexo))
     if c == 1 and sexo == 2:
         hvelho = nome
         hidade = idade
     if sexo == 1 and idade < 20:
-      m20 + 1
-    if sexo == 2 and hidade < idade:
+      m20 += 1
+    if sexo == 2 and idade > hidade:
         hvelho =nome
         hidade = idade
 
-print('A média de idade do grupo é : {}' .format( sidade / 4))
-print('O Homem mais velho é : {}'. format(hvelho))
-print('E temos {} mulheres com menos de 20 anos ' .format(m20))
+print('\033[32m A média de idade do grupo é : {}' .format( sidade / 4))
+print('\033[34m O Homem mais velho é : {} e tem {}anos'. format(hvelho, hidade))
+print('\033[31m E temos {} mulheres com menos de 20 anos ' .format(m20))
