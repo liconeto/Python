aea2d77748b01582a318dfef11ab9cf145474165
liconeto <benicio.neto9@gmail.com>
2021-07-02 00:24:34 -0300

    ex 071

--- pythonExercicios/ex071.py
+++ pythonExercicios/ex071.py
@@ -1,9 +1,6 @@
 import random
 from builtins import print
-from datetime import date
-from emoji import emojize
 from time import sleep
-from random import randint
 
 texto = ' \033[1;37mDesafio 071\033[m '
 print('{:*^50}'. format(texto))
@@ -20,28 +17,34 @@ conta50 = conta20 = conta10 = conta1 = 0
 print('.: \033[1;32mBem-vindo ao Caixa Eletrônico do CEV!\033[m :.')
 while True:
     try:
-        valor = int(input('Digite o valor do saque:'))
+        valor = int(input('Digite o valor do saque R$:'))
     except:
         print('valor inválido, Digite um número inteiro!')
     else:
         break
+total = valor
+ced = 50
+totced = 0
 
-conta50 = valor / 50
+while True:
+    if total >= ced:
+        total -= ced
+        totced += 1
+    else:
 
-if (valor % 50) > 20:
-    conta20 = (valor % 50) / 20
-if (valor % 20) > 10:
-    conta10 = (valor % 20) / 10
-if (valor % 10) >= 1:
-    conta1 = (valor % 10) / 1
 
+        if totced > 0:
+            print(f'Total de \033[1;31m{totced}\033[m cédulas de \033[34m{ced}\033[m')
+        if ced == 50:
+            ced = 20
+            totced = 0
+        elif ced == 20:
+            ced = 10
+            totced = 0
+        elif ced == 10:
+            ced =1
+            totced = 0
+        if total == 0:
+            break
 print(f'O valor do saque :{valor}')
-print('Calculando quantidade de notas ', end='')
-for c in range(5):
-    print('\033[31m.\033[m', end='')
-    sleep(0.4)
-print('')
-print(f'notas de 50 : \033[31m{int(conta50)}\033[m')
-print(f'notas de 20 : \033[34m{int(conta20)}\033[m')
-print(f'notas de 10 : \033[35m{int(conta10)}\033[m')
-print(f'notas de  1 : \033[36m{int(conta1)}\033[m')
+
