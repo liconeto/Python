085e00de9d8cc27294c59e1b52e11e9501832f85
liconeto <benicio.neto9@gmail.com>
2021-08-02 17:52:10 -0300

    aula23

--- pythonTeste/aula23.py
+++ pythonTeste/aula23.py
@@ -7,9 +7,15 @@ try:
     a = int(input('Numerador :'))
     b = int(input('Denominador: '))
     r = a / b
+except (ValueError, TypeError):
+    print(f'Tivemos um problema com os tipos de dados que você digitou ')
+except ZeroDivisionError:
+    print('Não é possivel dividir por zero!')
+except KeyboardInterrupt:
+    print('O usuário preferiu não informar os dados!')
 except Exception as erro:
-    print(f'Problema encontrado foi {erro.__class__}')
+    print(f'O erro informado foi {erro.__cause__}')
 else:
-    print(f'O resultado é {r}')
+    print(f'O resultado é {r:2f}')
 finally:
     print('VOLTE SEMPRE MUITO OBRIGADO!')
