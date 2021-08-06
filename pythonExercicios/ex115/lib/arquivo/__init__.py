725bda13d91f8234aa65a4f3ccb2f5ef593b7b7f
liconeto <benicio.neto9@gmail.com>
2021-08-05 22:36:59 -0300

    ex115

--- pythonExercicios/ex115/lib/arquivo/__init__.py
+++ pythonExercicios/ex115/lib/arquivo/__init__.py
@@ -6,3 +6,31 @@ except FileNotFoundError:
     arq = open('cursoemvideo.txt', 'w+')
 else:
     arq.close()
+
+
+def arquivo():
+    narquivo ='cursoemvideo.txt'
+    try:
+        arquivo = open(narquivo, 'r+')
+        print('Arquivo aberto!')
+    except FileNotFoundError:
+        arquivo = open(narquivo, 'w+')
+        print('Arquivo craido com sucesso!')
+    return narquivo
+
+
+
+def lerArquivo():
+    nomeArq = arquivo()
+    abrir = open(nomeArq, 'r+')
+    for linhas in abrir:
+        valores = linhas.split(';')
+        valores[1] = valores[1].replace('\n', '')
+        print(f'| {valores[0]:<30}{valores[1]:>8} |')
+
+
+def escreveArquivo(dado):
+    nomeArq = arquivo()
+    abrir = open(nomeArq, 'at+')
+    abrir.write(dado)
+    abrir.close()
--- pythonExercicios/ex115/sistema.py
+++ pythonExercicios/ex115/sistema.py
@@ -1,12 +1,18 @@
 from ex115.lib.interface import *
+from ex115.lib.arquivo import *
 from time import sleep
-
+arquivo()
 while True:
     resp = menu(['Ver pessoas cadastradas', 'Cadastrar nova pessoa', 'Sair do Sistema'])
     if resp == 1:
         cabecalho('Opção 1')
+        lerArquivo()
     elif resp == 2:
         cabecalho('Opção  2')
+        nome =str(input('Informe nome: '))
+        idade =int(input('Informe a idade: '))
+        dado = nome +';'+ str(idade)
+        escreveArquivo(dado+'\n')
     elif resp == 3:
         cabecalho('Saindo do sistema... Até logo!')
         break
