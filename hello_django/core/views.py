240b9b5d5668ce0cb51c2d8ce8b0a27fbde2d455
liconeto <benicio.neto9@gmail.com>
2021-08-30 15:51:50 -0300

    rotas Django

--- hello_django/core/views.py
+++ hello_django/core/views.py
@@ -4,3 +4,20 @@ from django.shortcuts import render, HttpResponse
 
 def hello(request, nome, idade):
     return HttpResponse(f'<h1>Hello {nome} de {idade} anos</h1>')
+
+
+def soma(request, n1, n2):
+    s = n1 + n2
+    return HttpResponse(f'A soma de {n1} + {n2} = {s}')
+
+def subtracao(request, n1, n2):
+    sub = n1 - n2
+    return HttpResponse(f'A subtração de {n1} - {n2} = {sub}')
+
+def multiplicacao(request, n1, n2):
+    multicacao = n1 * n2
+    return HttpResponse(f'A Multiplicação de {n1} * {n2} = {multicacao}')
+
+def divisao(request, n1, n2):
+    divisao = n1 / n2
+    return HttpResponse(f'A Divisão de {n1} / {n2} = {divisao}')
