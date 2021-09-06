aa639b14ae2e0e02af0ba957887a709056f7d240
liconeto <benicio.neto9@gmail.com>
2021-09-06 15:30:44 -0300

    redirect

--- agenda/core/views.py
+++ agenda/core/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from core.models import Evento
 # Create your views here.
 
@@ -8,3 +8,6 @@ def lista_eventos(request):
     #evento = Evento.objects.filter(usuario=usuario)
     dados = {'eventos':evento}
     return render(request, 'agenda.html', dados)
+
+#def index(request):
+#   return redirect('/agenda/')
