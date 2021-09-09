bbc36b872b282538f67ec4be390ebb0b721bb379
liconeto <benicio.neto9@gmail.com>
2021-09-09 12:06:22 -0300

    Incluido template para insercao de eventos da agenda

--- agenda/core/models.py
+++ agenda/core/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 
 class Evento(models.Model):
     titulo = models.CharField(max_length=100)
-    decricao = models.TextField(blank=True, null=True)
+    descricao = models.TextField(blank=True, null=True)
     data_evento = models.DateTimeField(verbose_name='Data do Evento')
     data_criacao = models.DateTimeField(auto_now=True, verbose_name='Data da Criação')
     usuario = models.ForeignKey(User, on_delete=models.CASCADE)
