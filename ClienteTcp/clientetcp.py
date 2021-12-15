8b52f6393c109693c9566d0829fba2287e035071
Benício Neto <assistec@FLIPPER.local>
2021-12-15 17:31:37 -0300

    TCP e UDP

---
+++ ClienteTcp/clientetcp.py
@@ -0,0 +1,28 @@
+import socket
+import sys
+
+def main():
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM, 0)
+    except socket.error as e:
+        print("A conexão falhou!!!")
+        print(f'Erro {e}')
+        sys.exit()
+
+    print("Socket criado com sucesso")
+
+    HostAlvo = input('Digite o host ou ip a ser conectado: ')
+    PortaAlvo = input('Digite a porta a ser conectada: ')
+
+    try:
+        s.connect((HostAlvo, int(PortaAlvo)))
+        print("cliente TCP conectado com sucesso no host "+HostAlvo+" e na porta: "+PortaAlvo )
+        s.shutdown(2)
+    except socket.error as e:
+        print("Não foi possivel conectar no Host: "+HostAlvo+" e Porta: "+PortaAlvo)
+        print(f"Erro: {e}")
+        sys.exit()
+
+
+if __name__ == "__main__":
+    main()
---
+++ ClienteeServidorUdp/clienteudp.py
@@ -0,0 +1,21 @@
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+print("cliente socket criado com sucesso!!!")
+
+host = 'localhost'
+porta = 5433
+mensagem = 'Olá servidor firmeza?'
+
+try:
+    print('Cliente:'+mensagem)
+    s.sendto(mensagem.encode(), (host, 5432))
+
+
+    dados, servidor = s.recvfrom(4096)
+    dados = dados.decode()
+    print("Cliente: "+ dados)
+finally:
+    print('Cliente: fechando a conexão')
+    s.close()
